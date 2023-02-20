8a97c14f2aaea346fda1fccba28c3cff48bc0e39
Cobra-117 <timothe.lacheroy@epitech.eu>
2023-02-20 12:09:30 +0100

    add roomManagement Class

--- Commands.cs
+++ Commands.cs
@@ -28,6 +28,8 @@ class Commands
 		}
 		else if (command[0] == "/list")
 			ListCommand(client);
+		else if (command[0] == "/listroom")
+			ListRoomCommand(client, room);
 		else if (command[0] == "/whisper" || command[0] == "/w")
 			WhisperCommand(client, command);
 		return client;
@@ -53,16 +55,28 @@ class Commands
 
 	public static void ListCommand(Client client)
     {
-		/*ServerUtilities.NotifyClient(client, "Connected clients:");
-		foreach (Client otherClient in TCPServerSample.clients)
-        {
-			ServerUtilities.NotifyClient(client, otherClient.pseudo);
-        }*/
+		ServerUtilities.NotifyClient(client, "Connected clients:");
+		foreach (Room room in TCPServerSample.rooms)
+		{
+			foreach (Client otherClient in room.clients)
+			{
+				ServerUtilities.NotifyClient(client, otherClient.pseudo);
+			}
+		}
     }
+
+	public static void ListRoomCommand(Client client, Room room)
+	{
+		ServerUtilities.NotifyClient(client, "Connected clients:");
+		foreach (Client otherClient in room.clients)
+		{
+			ServerUtilities.NotifyClient(client, otherClient.pseudo);
+		}
+	}
 
 	public static void WhisperCommand(Client client, string[] command)
 	{
-		/*Client target;
+		Client target;
 		string message = "";
 		if (command.GetLongLength(0) < 2)
 		{
@@ -74,18 +88,32 @@ class Commands
 			message += command[i];
 			message += " ";
         }
-		foreach (Client otherClient in TCPServerSample.clients)
+		foreach (Room room in TCPServerSample.rooms)
+			foreach (Client otherClient in room.clients) {
+				{
+					if (otherClient.pseudo == command[1])
+					{
+						ServerUtilities.NotifyClient(otherClient,
+							client.pseudo + " whispers " + message);
+						ServerUtilities.NotifyClient(client,
+							"You whisper to " + command[1] + " " + message);
+						return;
+					}
+				}
+			}
+		ServerUtilities.NotifyClient(client, "Target " + command[1] + " does not exist");
+	}
+
+	public static void JoinRoomCommand(Client client, string[] command)
+    {
+		if (command.GetLongLength(0) < 2)
 		{
-			if (otherClient.pseudo == command[1])
-            {
-				ServerUtilities.NotifyClient(otherClient,
-					client.pseudo + " whispers "+ message);
-				ServerUtilities.NotifyClient(client,
-					"You whisper to " + command[1]+ " " + message);
-				return;
-            }
+			ServerUtilities.NotifyClient(client, "Usage: /whisper <nickname> <message>");
+			return;
 		}
-		ServerUtilities.NotifyClient(client, "Target " + command[1] + " does not exist");*/
+		/*foreach (Room room in TCPServerSample.rooms)
+			foreach (Client otherClient in room.clients)*/
+
 	}
 
 }
